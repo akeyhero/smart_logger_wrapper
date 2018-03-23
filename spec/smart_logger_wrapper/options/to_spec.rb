e28381b72f26bc56ba843f5d495a3e2125d283ba
akihiro <akihiro@kats.la>
2018-03-23 20:33:38 +0900

    issue #1 - test the logger wrapper and the options

---
+++ spec/smart_logger_wrapper/options/to_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'logger'
+
+RSpec.describe SmartLoggerWrapper::Options::To do
+  let(:smart_logger_wrapper) { SmartLoggerWrapper.new(logger_stub) }
+  let(:logger_stub) { double(:logger) }
+  let(:severity_stub) { double(:severity) }
+
+  let(:handler_stub) { double(:handler) }
+  let(:message1) { ('a'..'z').to_a.sample(10).join }
+  let(:message2) { ('a'..'z').to_a.sample(10).join }
+
+  before do
+    allow(logger_stub).to receive(:log)
+    allow(logger_stub).to receive(:error)
+    allow(handler_stub).to receive(:puts)
+  end
+
+  context 'with a handler' do
+    subject! { smart_logger_wrapper.to(handler_stub).log(severity_stub, message1, message2) }
+
+    it 'calls #puts of the argument' do
+      expect(handler_stub).to have_received(:puts).with([message1, message2].join("\n")).once
+    end
+  end
+
+  context 'with no handler' do
+    subject! { smart_logger_wrapper.to.log(severity_stub, message1) }
+
+    it 'logs an exception' do
+      expect(logger_stub).to have_received(:error).with(include('No handler given')).once
+    end
+
+    it 'logs the original messages' do
+      expect(logger_stub).to have_received(:log).with(severity_stub, nil, message1).once
+    end
+  end
+end
