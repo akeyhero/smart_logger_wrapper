fa808f802bb3507a66ac215b5ba0045bab347eb4
akihiro <akihiro@kats.la>
2018-05-05 21:08:51 +0900

    issue #26 - filter position information with Rails.backtrace_cleaner

--- lib/smart_logger_wrapper/options/with_position.rb
+++ lib/smart_logger_wrapper/options/with_position.rb
@@ -1,11 +1,13 @@
 require 'logger'
 require 'smart_logger_wrapper/options/base'
 require 'smart_logger_wrapper/utils/path'
+require 'smart_logger_wrapper/utils/backtrace'
 
 class SmartLoggerWrapper < Logger
   module Options
     class WithPosition < Base
       include ::SmartLoggerWrapper::Utils::Path
+      include ::SmartLoggerWrapper::Utils::Backtrace
 
       def apply!(messages, arguments, severity, wrapper)
         enabled = arguments.first || true
@@ -13,7 +15,7 @@ class SmartLoggerWrapper < Logger
         # add 1 to `start` because this method dug the backtrace by 1
         location = caller_locations(wrapper.offset + APPLY_CALLER_STACK_DEPTH + 1, 1)
         prefix =
-          if location && location.length > 0
+          if location && location.length > 0 && location_important?(location)
             method_name = location[0].label
             path        = trim_dirname(location[0].absolute_path)
             lineno      = location[0].lineno
@@ -22,6 +24,12 @@ class SmartLoggerWrapper < Logger
             nil
           end
         messages.map! { |message| [prefix, message].compact.join(' ') }
+      end
+
+      private
+
+      def location_important?(location)
+        ! clean_backtrace(location.map(&:to_s)).empty?
       end
     end
 
